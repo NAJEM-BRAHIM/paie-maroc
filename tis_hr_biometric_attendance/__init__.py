761cb9c537c2278cabc2d683cee9cad5be7e5ccd
NAJEM-BRAHIM <SAHARA.INFO.SERVICES@GMAIL.COM>
2021-12-05 22:24:26 +0100

    Delete tis_hr_biometric_attendance directory

--- tis_hr_biometric_attendance/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# This module and its content is copyright of Technaureus Info Solutions Pvt. Ltd.
-# - © Technaureus Info Solutions Pvt. Ltd 2020. All rights reserved.
-
-from . import models
-from . import wizard
-from . import zk
